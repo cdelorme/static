895632cdd169a07a6e8695750c325ad839de5dc4
cdelorme <cdelorme@gmail.com>
2016-10-24 08:19:07 -0400

    switch to gonf for configuration

--- cmd/staticmd/main.go
+++ cmd/staticmd/main.go
@@ -5,9 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/cdelorme/go-log"
-	"github.com/cdelorme/go-maps"
-	"github.com/cdelorme/go-option"
-
+	"github.com/cdelorme/gonf"
 	"github.com/cdelorme/staticmd"
 )
 
@@ -27,17 +25,15 @@ func configure() generator {
 		Output: filepath.Join(cwd, "public/"),
 	}
 
-	appOptions := option.App{Description: "command line tool for generating deliverable static content"}
-	appOptions.Flag("template", "path to the template file", "--template", "-t")
-	appOptions.Flag("input", "path to the markdown files", "--input", "-i")
-	appOptions.Flag("output", "path to place generated content", "--output", "-o")
-	appOptions.Flag("book", "combine all content into a single file", "--book", "-b")
-	appOptions.Flag("relative", "use relative paths instead of absolute paths", "--relative", "-r")
-	appOptions.Example("-t template.tmpl -i . -b")
-	appOptions.Example("-t template.tmpl -i src/ -o out/ -r")
-	flags := appOptions.Parse()
-
-	maps.To(smd, flags)
+	g := &gonf.Gonf{Description: "command line tool for generating deliverable static content", Configuration: smd}
+	g.Add("template", "path to the template file", "STATICMD_TEMPLATE", "--template", "-t")
+	g.Add("input", "path to the markdown files", "STATICMD_INPUT", "--input", "-i")
+	g.Add("output", "path to place generated content", "STATICMD_OUTPUT", "--output", "-o")
+	g.Add("book", "combine all content into a single file", "STATICMD_BOOK", "--book", "-b")
+	g.Add("relative", "use relative paths instead of absolute paths", "STATICMD_RELATIVE", "--relative", "-r")
+	g.Example("-t template.tmpl -i . -b")
+	g.Example("-t template.tmpl -i src/ -o out/ -r")
+	g.Load()
 
 	return smd
 }
